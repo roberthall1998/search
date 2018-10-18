760b39f5b79f193ec99af281a994673e652c548e
roberthall01 <r.hall80@hotmail.com>
2018-10-18 12:28:58 +0100

    fixed highlighting and length issues

--- webapp/src/app/data.service.ts
+++ webapp/src/app/data.service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Document } from './document.model';
-import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { controlNameBinding } from '@angular/forms/src/directives/reactive_directives/form_control_name';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Properties
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   //List of documents to be displayed on the page
   docs: Document[] = [];
+  //Boolean to identify if page has loaded and array is still empty
+  empty:boolean = false;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Constructor
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   constructor(private http: HttpClient) {
     this.updateSearch(undefined);
   }
 
-  empty:boolean = false;
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //  Methods
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //Called on each search request
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Called on each search request to call the solr API and return the relevant results
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   updateSearch(keyword: string){
     var localKeyword = keyword;
     
@@ -27,26 +39,36 @@ export class DataService {
       localKeyword = '*:*';
     }
 
+    //parse the query into an array of queries
+    var queries = this.parseQuery(keyword);
+
     //empty doc array
     this.docs = [];
+    //indicate loading has started
     this.empty = false;
     //send API request with new keyword
     const url = 'http://thesearch.kainos.com:8984/solr/search/select?q=' + localKeyword.replace(' ','%20');
-    console.log(url);
 
     //parse response and push each into docs array
     this.http.get(url + "&rows=10000").subscribe((data: any) => {
       for(let doc of data.response.docs){
-        var content = this.parseContent(doc.content, keyword);
-        if(content.length>0){
-          this.docs.push(new Document(this.parseID(doc.id), doc.title, content));
-        }
+        var id = this.parseID(doc.id);
+        var content = this.parseContent(doc.content, queries);
+        
+          this.docs.push(new Document(id, doc.title, content));
+        
       }
+
+      //indicate loading has finished
       if(this.docs.length == 0){
         this.empty = true;
       }
     });
   }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // To convert the id to a sharepoint url
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   public parseID(id: string){
     var start = id.indexOf('/docs/');
@@ -54,80 +76,104 @@ export class DataService {
     return path;
   }
 
-  //Convert the content JSON object to a string and highlight the keyword
-  public parseContent(content: string, query: string){
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // To convert the content into a list of highlighted search results
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  public parseContent(content: string, queries: string[]){
     //Remove all \n and \t from the content
     var contentString = JSON.stringify(content).replace(/\\t./g, '').replace(/\\n/g, '');
     //Remove the square brackets and speech marks at the start and end of the content
     var betterContentString = contentString.replace('[" ','').replace('"]','');
-    //generate a list of search results.
-    var results: string[] = [];
-    if(!query){
+
+    if(!queries){
       //if no query just display the first 2000 chars
-      results = [betterContentString.substring(0,2000)];
+      return [betterContentString.substring(0,2000)];
     }else{
-      //display the first 5 sentences to contain the query
-      results = this.searchResults(betterContentString, query);
+      var highlighted:string[] = [];
+      var sentences = this.paragraphToSentences(content, queries);
+      //for every sentence, store it locally then run it through highlighting with each query, then push
+      for(let s of sentences){
+        var plain = s;
+        for(let q of queries){
+          plain = this.highlight(plain,q);
+        }
+        highlighted.push(plain);
+      }
+      
+      return highlighted;
     }
-    //highlight results
-    var list: string[] = [];
-    for(let i=0; i<results.length; i++){
-      list[i] = this.highlight(results[i],query);
-    }
-    return list;
   }
 
-  public searchResults(content: string, query: string){
-    var sentences: string[] = [];
-    var queries: string[] = [];
-    //If speech marks at start and end 
-    if(query.charAt(0) == '"' && query.charAt(query.length-1) == '"'){
-      queries.push(query.replace (/"/g,''));
-      console.log('quotes detected');
-    }else{
-      queries = query.split(' ');
-      console.log('no quotes splitting string');
-    }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // To convert the content into a list of highlighted search results
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //Split the paragraph at each full stop
-    var split = content.split('.');
+  public paragraphToSentences(paragraph: string, queries: string[]){
+    //empty list to store results
+    var results: string[] = [];
+    
+    var sentences = paragraph.toString().split('.');
 
-    //If the sentence contains any of the keywords add to array
-    for(let i=0; i<split.length; i++){
-      if(this.containsQueries(split[i], queries)){
-        sentences.push(split[i]);
+    var count = 0;
+
+    //For each sentence check query up to max 5
+    for(let sentence of sentences){
+      for(let q of queries){
+        if(this.contains(sentence,q)){
+          results.push(sentence.substring(0,1000));
+          count++;
+          if(count == 5){
+            return results;
+          }
+        }
       }
     }
     //return array
-    return sentences.slice(0,5);
+    return results;
   }
 
-  //True if word in sentence, false if not
-  public contains(sentence: string, query: string){
-    if(sentence.toLowerCase().search(query.toLowerCase())>-1){
-      return true;
-    }
-    return false;
-  }
-
-  public containsQueries(s:string, queries:string[]){
-    for(let q of queries){
-      if(this.contains(s, q)){
-        return true;
-      }
-    }
-    return false;
-  }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // True if string contains query, false if not
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   //Add bold (<b>test</b>) tags around the keyword
   public highlight(content: string, query: string) {
-    if(!query) {
-        return content;
-    }
-    var parsedQuery = query.replace(/"/g,"").replace(' ', '\\s');
-    return content.replace(new RegExp(parsedQuery, "gi"), match => {
+    return content.toString().replace(new RegExp(query, "gi"), match => {
         return '<b>' + match + '</b>';
     });
-    
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // True if string contains query, false if not
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  public contains(sentence: string, query: string){
+    if(sentence.toLowerCase().indexOf(query.toLowerCase())>-1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // To convert the content into a list of highlighted search results
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  public parseQuery(query: string){
+    if(!query){
+      return undefined;
+    }
+    //empty list to store queries
+    var queries: string[] = [];
+    //if query has quotes just store full phrase as query
+    if(query.charAt(0) == '"' && query.charAt(query.length-1) == '"'){
+      queries.push(query.replace (/"/g,''));
+    //otherwise split the query up
+    }else{
+      queries = query.split(' ');
+    }
+
+    return queries;
   }
 }
